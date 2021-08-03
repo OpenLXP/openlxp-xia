e71c70d7ffc2f2690cb3e38cabe9b003ea606335
SHGODE <SHGODE@DELOITTE.com>
2021-08-03 17:12:26 -0400

    Package version 1.0.17.

--- app/openlxp_xia_project/settings.py
+++ app/openlxp_xia_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    'openlxp_django_xia',
 ]
 
 MIDDLEWARE = [
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
+        'HOST': 'localhost',
         'PORT': 3306,
     }
 }
